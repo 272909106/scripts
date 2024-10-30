41e51deef0999e93f39d030e6436c2ab626cc699
272909106 <272909106@qq.com>
2024-10-30 13:47:21 +0800

    Update colletcd-all.sh
    增加备份hosts文件；
    添加注释；

--- kubernetes/k8s_collect/colletcd-all.sh
+++ kubernetes/k8s_collect/colletcd-all.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -xe
+
+currentPath=$PWD
+maintenancePath='/data/maintenance'
+dockerConfig='/etc/docker/daemon.json'
+keepalivedCfg='/etc/keepalived/keepalived.conf'
+haproxyCfg='/etc/haproxy/haproxy.cfg'
+
+#清理路径并创建、进入路径
 cleanPath(){
 	if [ ! -d $1 ]
         then 
@@ -9,30 +17,29 @@ cleanPath(){
 	  mkdir -p $1 && cd $1
         fi
 }
+
+#拷贝路径
 copyPath(){
 if [ -d $1 ];then
 	cp -r $1 .
 fi
 }
 
+#拷贝文件
 copyFile(){
 if [ -f $1 ];then
         cp $1 .
 fi
 }
 
+#装饰器函数
 decorator() {
   cleanPath $2
   $1
   cd $currentPath
 }
-currentPath=$PWD
-maintenancePath='/data/maintenance'
-dockerConfig='/etc/docker/daemon.json'
-keepalivedCfg='/etc/keepalived/keepalived.conf'
-haproxyCfg='/etc/haproxy/haproxy.cfg'
 
-
+#备份adp
 bakTecoAdp(){
 for n in `kubectl get ns |grep -v NAME |awk  '{print $1}'`;
 do mkdir $n ;cd $n
@@ -47,15 +54,18 @@ done
 find ./ -type d -empty |xargs rmdir
 }
 
+#备份docker config
 bakDockerConfig(){
 copyFile $dockerConfig 
 }
 
+#备份维护脚本
 bakMaintenance(){
 copyPath $maintenancePath/scripts
 copyPath $maintenancePath/bin
 }
 
+#备份keepalived 和 haproxy
 bakKeepalivedHaproxy(){
 copyFile $keepalivedCfg
 scp  adp-master-2:/etc/keepalived/keepalived.conf keepalived-master-0002.conf
@@ -63,12 +73,23 @@ scp  adp-master-3:/etc/keepalived/keepalived.conf keepalived-master-0003.conf
 copyFile $haproxyCfg 
 }
 
+#备份crontab定时任务
 bakCrontab(){
 crontab -l >./crontab
 }
 
+#备份host
+backHost(){
+cat /etc/hosts >./hosts
+}
+
+main(){
 decorator bakTecoAdp kubesphere-backup-yaml
 decorator bakDockerConfig docker-config
 decorator bakMaintenance maintenance
 decorator bakKeepalivedHaproxy keepalived-haproxy
 bakCrontab
+backHost
+}
+
+main
